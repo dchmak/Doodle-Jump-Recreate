9f3d0d66b9695561239c81fadfd73d95488fd52d
Daniel <32817670+dchmak@users.noreply.github.com>
2018-05-05 15:48:09 -0400

    Added basic structures
    - added sprites
    - added basic movements

---
+++ Doodle Jump Recreate/Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Vector2 spawnPos;
+    public GameObject playerPrefab;
+    public GameObject platformPrefab;
+
+	void Start () {
+        spawnPlayer();
+        spawnPlatform();
+	}
+
+    void spawnPlayer() {
+        Instantiate(playerPrefab, spawnPos, Quaternion.identity);
+    }
+
+    void spawnPlatform() {
+
+    }
+}
---
+++ Doodle Jump Recreate/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public int horizontalSpeed;
+    public int bounceForce;
+
+    Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+	void Update () {
+        transform.Translate(Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime, 0, 0);
+	}
+
+    void OnCollisionEnter2D (Collision2D collision) {
+        //Debug.Log(collision.gameObject.tag);
+
+        if (collision.gameObject.tag == "Platform") {
+            rb.AddForce(Vector2.up * bounceForce);
+        }
+    }
+}
