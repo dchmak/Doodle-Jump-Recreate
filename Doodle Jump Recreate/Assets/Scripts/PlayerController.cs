b224efc7765c33ef28d53085d5a7baf52c5ea834
Daniel <32817670+dchmak@users.noreply.github.com>
2018-05-07 11:36:17 -0400

    bug fixes
    + falling feel more natural
    + multistage boosting is fixed (I really hope)

--- Doodle Jump Recreate/Assets/Scripts/PlayerController.cs
+++ Doodle Jump Recreate/Assets/Scripts/PlayerController.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer))]
 public class PlayerController : MonoBehaviour {
 
     public int horizontalSpeed;
     public int bounceForce;
 
+    [Range(1f, 5f)] public float fallMultiplier;
+
     Rigidbody2D rb;
     SpriteRenderer sr;
 
-    void Update() {
-        transform.Translate(Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime, 0, 0);
+    private bool jumpRequest;
 
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
         GameObject cam = GameObject.FindGameObjectWithTag("MainCamera");
         float height = Camera.main.orthographicSize;
         float worldScreenWidth = height * 2f / Screen.height * Screen.width;
@@ -37,23 +45,30 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
-    void OnCollisionEnter2D (Collision2D collision) {
-        if (rb == null) rb = GetComponent<Rigidbody2D>();
-        if (sr == null) sr = GetComponent <SpriteRenderer>();
-        
+    void OnCollisionEnter2D(Collision2D collision) {
         float playerPosY = transform.position.y - sr.bounds.size.y / 2;
         float platformPosY = collision.transform.position.y;
 
         if (collision.gameObject.tag == "Platform") {
-            if (playerPosY >= platformPosY) {
+            if (playerPosY >= platformPosY && rb.velocity == Vector2.zero) {
                 rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
             }
         }
 
         if (collision.gameObject.tag == "Broken Platform") {
-            if (playerPosY >= platformPosY) {
+            if (playerPosY >= platformPosY && rb.velocity == Vector2.zero) {
                 collision.gameObject.SetActive(false);
             }
         }
     }
+
+    void FixedUpdate() {
+        transform.Translate(Input.GetAxis("Horizontal") * horizontalSpeed * Time.deltaTime, 0, 0);
+
+        if (rb.velocity.y < 0) {
+            rb.gravityScale = fallMultiplier;
+        } else {
+            rb.gravityScale = 1f;
+        }
+    }    
 }
